5cfd248f3d711e0e3588405595b9b37a0459c40e
Sunshine GameStudio <sunshinegamestudio@users.noreply.github.com>
2018-11-14 04:51:14 +0100

    First implementation

---
+++ Assets/Scripts/CoreActions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CoreActions : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Accept()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void Decline()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public float torque;
+
+    private Rigidbody rb;
+
+    public VirtualJoystick moveJoystick;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        /* Code for movement -> remove this later
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+        */
+
+        /* Code for VirtualJoystick -> Adapt to code for snowboarding game
+        Vector3 dir = Vector3.zero;
+
+        dir.y = Input.GetAxis ("Horizontal");
+        dir.x = Input.GetAxis ("Vertical");
+
+        if (dir.magnitude > 1)
+        {
+            dir.Normalize();   
+        }
+
+        if (moveJoystick.InputDirection != Vector3.zero)
+        {
+            dir = moveJoystick.InputDirection;
+        }
+
+        rb.AddTorque(transform.right * torque * dir.y);
+        rb.AddTorque(transform.up * torque * dir.x);
+        */
+
+        
+        float turn_v = Input.GetAxis("Vertical");
+
+        rb.AddTorque(transform.right * torque * turn_v);
+
+        float turn_h = Input.GetAxis("Horizontal");
+
+        rb.AddTorque(transform.up * torque * turn_h);
+        
+
+        // Jump
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
